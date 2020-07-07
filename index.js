b1607c857a7ed441371b0cc4e3c7e882011b6314
amandafdal <amandafdal@gmail.com>
2020-07-06 21:56:50 -0300

    mdLinks function

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+const mdLinks = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        return reject('err')
+      } else{
+        const regex = /\[.[^[\](\)]*\]\(http[^#][^[\](\)]*\)/gm;
+        const dataMatch = data.match(regex);
+        if (!dataMatch) {
+          return resolve('');
+        } else {
+          let arr = [];
+          dataMatch.map(ref => {
+            const refSplit = ref.split('](')
+            const obj = {
+              file: path,
+              text: refSplit[0].replace('[', ''),
+              href: refSplit[1].replace(')', ''),
+            };
+            arr.push(obj);
+          })
+          return resolve(arr)
+        }
+      }
+    });
+  })
 };
+
+module.exports = mdLinks;
