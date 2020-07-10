3851f9bac48c4e879d64262f46ef26007568ad91
amandafdal <amandafdal@gmail.com>
2020-07-10 18:44:53 -0300

    refatoração index

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const fs = require('fs');
+const regex = /\[.[^[\](\)]*\]\(http[^#][^[\](\)]*\)/gm;
 
 const mdLinks = (path) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         return reject('Unable to find path. Insert full path. E.g.: ./some/example.md')
-      } else{
-        const regex = /\[.[^[\](\)]*\]\(http[^#][^[\](\)]*\)/gm;
-        const dataMatch = data.match(regex);
-        if (!dataMatch) {
+      } else if (!data.match(regex)) {
           return resolve('');
         } else {
           let arr = [];
-          dataMatch.map(ref => {
+          data.match(regex).map(ref => {
             const refSplit = ref.split('](')
             const obj = {
               file: path,
@@ -23,7 +21,6 @@ const mdLinks = (path) => {
           })
           return resolve(arr)
         }
-      }
     });
   })
 };
